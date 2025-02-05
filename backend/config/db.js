982ad9139557f5c6d236be0dae95ff7b01bf08ab
Aman Kumar Singh Kushwaha <amansinghkushwaha2003@gmail.com>
2025-02-05 15:37:45 +0530

    prod db certificate config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,16 @@
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
+const fs = require('fs');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('./prod-ca-2021.crt').toString()
+  }
 });
 
-pool.connect((err, _, release) => {
+pool.connect((err, client, release) => {
   if (err) {
     console.error('Error acquiring client', err.stack);
     return;
